312b24a4b19cff9660e13c1de787ecf3601eeb66
Harsh <hagarwal@velsof.com>
2022-11-04 19:56:55 +0530

    CSS issue fixed.

--- src/resources/views/components/layout.blade.php
+++ src/resources/views/components/layout.blade.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Alcobrew</title>
-    <link href="{{asset('theme/assets/css/bootstrap.min.css')}}" rel="stylesheet">
+    <link href="{{asset('disclaimer_assets/css/bootstrap.min.css')}}" rel="stylesheet">
     <link rel="stylesheet" href="{{asset('disclaimer_assets/css/font-awesome.min.css')}}">
     <link rel="stylesheet" href="{{asset('assets/css/css2')}}">
   </head>
